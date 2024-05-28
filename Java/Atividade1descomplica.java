f1d0744c175c87ad2800e6f54ecd4392016d2592
Natalia <nasousa77@gmail.com>
2024-05-28 20:13:50 -0300

    projeto

---
+++ Java/Atividade1descomplica.java
@@ -0,0 +1,20 @@
+// nome da classe
+class Atividade1descomplica
+{
+ // modúlo principal com a entrada pela linha de comando
+    public static void main (String entrada[])
+    {
+        //declarar as variaveis
+        int inteiro = 34;
+        char caracter = 'F';
+        double real = 1.58;
+        String frase = "Natália Eliz";
+        boolean VF = true;
+        
+        if (VF == true)
+        {
+            System.out.println("Eu sou a(o) " + frase + ", tenho " + inteiro + " anos e tenho " + real + "m de altura.");
+        }
+        System.exit(0);
+    }
+}
---
+++ Java/Atividade3descomplica.java
@@ -0,0 +1,18 @@
+
+class Atividade3descomplica
+{
+ 
+    public static void main (String entrada[])
+    {
+        
+	String nome = "Natália Eliz";
+	char caracter = 'F';
+	int inteiro = 34;
+        double peso = 47.5;
+        String curso = "Análise e Desenvolvimento de Sistemas";
+       	
+	System.out.println("Eu sou a " + nome + ", do sexo " + caracter + " e estou no curso de " + curso + ", tenho " + inteiro + " anos e peso " + peso + "kg.");
+        
+        System.exit(0);
+    }
+}
