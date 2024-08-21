9e1662596aa53524cb0715c2a44c792306afa14e
Natalia <nasousa77@gmail.com>
2024-08-21 16:33:30 -0300

    Atual

---
+++ Html/minha_pagina.js
@@ -0,0 +1,7 @@
+let vari = require('readline-sync');
+let name = vari.question("Qual e o seu nome?:\n")
+console.log(`Olá, ${name}`);
+
+
+let idaded = vari.question("Qual e a sua idade?:\n")
+console.log(`Voce tem ${idaded} anos`);
